528bbd852c29d117b6eb31a864e9feae64e709a2
Shlok-Zanwar <Shlok-Zanwar@users.noreply.github.com>
2020-12-29 13:25:31 +0530

    6_ReverseTheArray

--- Array/6_ReverseTheArray.cpp
+++ Array/6_ReverseTheArray.cpp
@@ -1,6 +1,39 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+void reverseArray(int arr[], int len){
+    int i;
+    for(i = 0; i < len/2; i ++){
+        // swap(arr[i], arr[len - i - 1]);
+
+        // Swap in O(0) space complexity
+        arr[i] = arr[i] + arr[len - i - 1];
+        arr[len - i - 1] = arr[i] - arr[len - 1 - i];
+        arr[i] = arr[i] - arr[len - i - 1];
+
+    }
+}
+
+void printArray(int arr[], int len){
+    int i;
+    for(i = 0; i < len; i++){
+        cout << arr[i] << " ";
+    }
+}
+
 int main(){
+    int len, i;
+    cout << "Length of Array : ";
+    cin >> len;
+
+    int arr[len];
+
+    cout << "Input elements  : ";
+    for(i = 0; i < len; i++){
+        cin >> arr[i];
+    }
     
+    reverseArray(arr, len);
+    cout << "Reversed Array  : ";
+    printArray(arr, len);
 }
