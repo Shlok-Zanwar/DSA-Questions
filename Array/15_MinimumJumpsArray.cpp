7f8dc45a036cac8d7adcce8fdf59b7c8ae2a6022
Shlok-Zanwar <Shlok-Zanwar@users.noreply.github.com>
2021-01-01 09:02:57 +0530

    Create 15_MinimumJumpsArray.cpp

---
+++ Array/15_MinimumJumpsArray.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void printArray(int arr[], int len){
+    int i;
+    for(i = 0; i < len; i++){
+        cout << arr[i] << " ";
+    }
+}
+
+
+void inputArray(int arr[], int len){
+    int i;
+    for(i = 0; i < len; i++){
+        cin >> arr[i];
+    }
+}
+
+
+int minimumNoOfJumps(int arr[], int len){
+    int i = 0, maxJumpsIndex, maxJumps = 0, j, totalJumps = 0, jumpsLeft;
+
+
+    while (i < len-1){
+        jumpsLeft = arr[i] - 1;
+        
+        if(arr[i] + i >= len - 1){
+            totalJumps ++;
+            break;
+        }
+
+        for(j = i + 1; j <= i + arr[i] ; j ++){
+            if(arr[j] - jumpsLeft >= maxJumps ){
+                maxJumpsIndex = j;
+                maxJumps = arr[i] - jumpsLeft;
+            }
+            jumpsLeft --;
+        }
+        totalJumps ++;
+        i = maxJumpsIndex;
+    }
+
+    return totalJumps;
+}
+
+
+int main(){
+    int len, i;
+    cout << "Length of Array : ";
+    cin >> len;
+
+    int arr[len];
+    cout << "Input elements  : ";
+    inputArray(arr, len);
+    
+}
