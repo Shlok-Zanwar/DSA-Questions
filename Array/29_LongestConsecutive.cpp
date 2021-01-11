f5735516f6fbf653ca835d4dd875db31c117d64c
Shlok-Zanwar <Shlok-Zanwar@users.noreply.github.com>
2021-01-11 21:00:57 +0530

    Create 29_LongestConsecutive.cpp

---
+++ Array/29_LongestConsecutive.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void inputArray(int arr[], int len){
+    int i;
+    for(i = 0; i < len; i++){
+        cin >> arr[i];
+    }
+}
+
+
+int findLongestConseqSubseq(int arr[], int len){
+    int hashArr[100001] = {0}, i, longestCount = 1, currCount = 0;
+    for(i = 0; i < len; i ++){
+        hashArr[arr[i]] ++;
+    }
+
+    for(i = 0; i < 100001; i ++){
+        if(hashArr[i] == 0){
+            longestCount = max(longestCount, currCount);
+            currCount = 0;
+        }
+        else{
+            currCount ++;
+        }
+    }
+
+    longestCount = max(longestCount, currCount);
+
+    return longestCount;
+
+}
+
+
+int main(){
+    int len, i;
+    cout << "Length of Array : ";
+    cin >> len;
+
+    int arr[len];
+    cout << "Input elements  : ";
+    inputArray(arr, len);
+    
+}
